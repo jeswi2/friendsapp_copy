8620814d54b724aa35c02c1f4477762c5df60a82
jeswi2 <jeswimathewm@gmail.com>
2022-10-14 22:01:01 +0530

    se

---
+++ src/app/api.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  viewfriends=()=>{
+    return this.http.get("http://dummyapifriends.herokuapp.com/view")
+  }
+ 
+}
